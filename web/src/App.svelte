62338388e75049d6657969a6dc753b91e3cc7fe0
Satsuki Akiba <anosatsuk124@gmail.com>
2022-08-01 18:23:37 +0900

    update frontend

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -1,14 +1,49 @@
 <script lang="ts">
-  const ws = new WebSocket("wss://localhost:8080");
+  const ws = new WebSocket("ws://127.0.0.1:3000");
 
-  const wsSend = () => {
-    ws.send("Hello world");
+  ws.onerror = (e) => {
+    console.log(`failed to connect:${e}`);
+    console.log(e);
   };
+
+  const morseDash = () => {
+    ws.send(
+      JSON.stringify({
+        dash: true,
+        dot: false,
+      })
+    );
+  };
+
+  const morseDot = () => {
+    ws.send(
+      JSON.stringify({
+        dash: false,
+        dot: true,
+      })
+    );
+  };
+
+  const morseMessage = (ws.onmessage = async (e) => {
+    const text = await e.data.text();
+    const object = JSON.parse(text);
+    console.log(`${text}`);
+    if (object.dash) {
+      message = message + "-";
+    } else {
+      message = message + ".";
+    }
+  });
+  $: message = "";
 </script>
 
 <main>
-  <h1>Vite + Svelte</h1>
-  <button on:click={wsSend}>Send "Hello world"</button>
+  <h1>Amateur Radio Emulator</h1>
+  <button on:click={morseDash}>-</button>
+  <button on:click={morseDot}>.</button>
+  <div>
+      <p>{message}</p>
+  </div>
 </main>
 
 <style>
